b40d72b624af1a5637b9119182a1b1e887ef86fe
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-06 16:36:55 +0900

    feat: 유저 비즈니스 로직 추가

--- src/main/java/com/example/kotsuexample/service/UserService.java
+++ src/main/java/com/example/kotsuexample/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.kotsuexample.service;
 
-import com.globalin.kotsukotsu.repository.UserRepository;
+import com.example.kotsuexample.dto.*;
+import com.example.kotsuexample.entity.User;
+import com.example.kotsuexample.exception.*;
+import com.example.kotsuexample.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,90 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+
+    public Boolean isEmailDuplicated(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public void join(JoinRequest joinRequest) {
+        String nickname = joinRequest.getNickname();
+
+        boolean isExistNickname = userRepository.existsByNickname(nickname);
+
+        if (isExistNickname) {
+            throw new ExistNicknameException("존재하는 닉네임입니다!");
+        }
+
+        User newUser = joinRequest.toEntity();
+        userRepository.save(newUser);
+    }
+
+    public LoginResponse login(LoginRequest loginRequest) {
+        String email = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        User foundedUser = userRepository.findByEmailAndPassword(email, password)
+                .orElseThrow(() -> new UserLoginException("이메일 또는 비밀번호가 잘못되었습니다!"));
+
+        return LoginResponse.builder()
+                .id(foundedUser.getId())
+                .email(foundedUser.getEmail())
+                .nickname(foundedUser.getNickname())
+                .profileImage(foundedUser.getProfileImage())
+                .build();
+    }
+
+    public UserInfoDTO getUserInfo(Integer userId) {
+
+        User foundedUser = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundByIdException("아이디 값에 따른 유저가 조회되지 않습니다."));
+
+        return UserInfoDTO.builder()
+                .phoneNumber(foundedUser.getPhoneNumber())
+                .email(foundedUser.getEmail())
+                .password(foundedUser.getPassword())
+                .nickname(foundedUser.getNickname())
+                .question(foundedUser.getQuestion())
+                .answer(foundedUser.getAnswer())
+                .profileImage(foundedUser.getProfileImage())
+                .profileMessage(foundedUser.getProfileMessage())
+                .createdAt(foundedUser.getCreatedAt())
+                .build();
+    }
+
+    public UserInfoDTO updateUserInfo(UserInfoDTO userInfoDTO) {
+        String email = userInfoDTO.getEmail();
+
+        User foundedUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundByEmailException("이메일에 따른 유저가 조회되지 않습니다."));
+
+        User updatedUser = userInfoDTO.toUpdatedEntity(foundedUser.getId());
+        userRepository.save(updatedUser);
+
+        return userInfoDTO;
+    }
+
+    public ResponseMessage getUserEmail(String phoneNumber, String answer) {
+        User foundedUser = userRepository
+                .findByPhoneNumberAndAnswer(phoneNumber, answer)
+                .orElseThrow(() -> new UserNotFoundByPhoneNumberAndAnswer("전화번호 또는 답변이 잘못되었습니다."));
+
+        String email = foundedUser.getEmail();
+
+        return ResponseMessage.builder()
+                .message(email)
+                .build();
+    }
+
+    public ResponseMessage getUserPassword(String email, String answer) {
+        User foundedUser = userRepository
+                .findByEmailAndAnswer(email, answer)
+                .orElseThrow(() -> new UserNotFoundByEmailAndAnswer("이메일 또는 답변이 잘못되었습니다."));
+
+        String password = foundedUser.getPassword();
+
+        return ResponseMessage.builder()
+                .message(password)
+                .build();
+    }
 }
