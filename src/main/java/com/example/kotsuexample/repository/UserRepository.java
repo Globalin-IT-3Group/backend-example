a57fa100ea60da4e5eb6fb899754a1266717f37b
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-06 16:36:48 +0900

    feat: 유저 리포지토리 메서드 추가

--- src/main/java/com/example/kotsuexample/repository/UserRepository.java
+++ src/main/java/com/example/kotsuexample/repository/UserRepository.java
@@ -1,7 +1,16 @@
 package com.example.kotsuexample.repository;
 
-import com.globalin.kotsukotsu.entity.User;
+import com.example.kotsuexample.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+    Optional<User> findByEmailAndPassword(String email, String password);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByPhoneNumberAndAnswer(String phoneNumber, String answer);
+    Optional<User> findByEmailAndAnswer(String email, String answer);
 }
