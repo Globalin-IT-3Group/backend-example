4bd743d689d47855f056b4914140f04bb703faea
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 17:15:26 +0900

    feat: request에서 속성 가져오는 커스텀 어노테이션 추가

---
+++ src/main/java/com/example/kotsuexample/config/CurrentUserArgumentResolver.java
@@ -0,0 +1,34 @@
+package com.example.kotsuexample.config;
+
+import com.example.kotsuexample.exception.user.UserUnauthorizedException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class)
+                && parameter.getParameterType().equals(Integer.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) {
+
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Integer userId = (Integer) request.getAttribute("userId");
+
+        if (userId == null) {
+            throw new UserUnauthorizedException("로그인이 필요합니다.");
+        }
+
+        return userId;
+    }
+}
