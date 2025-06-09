d39afabc92b67e9f4de689d99237c11db371774d
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 20:29:55 +0900

    feat: sse 서비스 클래스 추가

---
+++ src/main/java/com/example/kotsuexample/service/SseService.java
@@ -0,0 +1,37 @@
+package com.example.kotsuexample.service;
+
+import com.example.kotsuexample.entity.enums.NotificationType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class SseService {
+
+    private final Map<Integer, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    public SseEmitter subscribe(Integer userId) {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        emitters.put(userId, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(userId));
+        emitter.onTimeout(() -> emitters.remove(userId));
+        return emitter;
+    }
+
+    public void send(Integer receiverId, NotificationType type, String message) {
+        SseEmitter emitter = emitters.get(receiverId);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name(type.name().toLowerCase()) // ex: "friend", "study", "system"
+                        .data(message));
+            } catch (IOException e) {
+                emitters.remove(receiverId);
+            }
+        }
+    }
+}
