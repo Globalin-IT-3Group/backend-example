af5e6a6ad217bef31ac1d7ae0e5ce0f6c0998547
jody816 <81346079+jody816@users.noreply.github.com>
2025-06-09 12:20:54 +0900

    feat: redis 메서드 추가

--- src/main/java/com/example/kotsuexample/config/redis/RedisUtil.java
+++ src/main/java/com/example/kotsuexample/config/redis/RedisUtil.java
@@ -1,4 +1,33 @@
 package com.example.kotsuexample.config.redis;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
 public class RedisUtil {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void saveAccessToken(String key, String value, Duration timeout) {
+        redisTemplate.opsForValue().set(key, value, timeout);
+    }
+
+    public String getAccessToken(String key) {
+        return Optional.of(redisTemplate.opsForValue().get(key))
+                .map(Object::toString)
+                .orElse(null);
+    }
+
+    public void deleteAccessToken(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
 }
