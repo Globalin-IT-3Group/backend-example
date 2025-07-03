e29e1f2a29232145a31ade08909210c50557d692
jody816 <81346079+jody816@users.noreply.github.com>
2025-07-03 20:49:30 +0900

    hot fix

--- src/main/java/com/example/kotsuexample/controller/SseController.java
+++ src/main/java/com/example/kotsuexample/controller/SseController.java
@@ -3,6 +3,7 @@ package com.example.kotsuexample.controller;
 import com.example.kotsuexample.config.CurrentUser;
 import com.example.kotsuexample.service.SseService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/sse")
+@Slf4j
 public class SseController {
 
     private final SseService sseService;
 
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@CurrentUser Integer userId) {
+        log.info("🔔 SSE 연결 요청: userId = {}", userId);
         return sseService.subscribe(userId);
     }
 }
